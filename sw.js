dd3ca771981583bc532bddb171b5c849c05de385
LMP75006 <contact@lamaisonpropre.fr>
2026-04-15 13:55:07 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,24 +1,20 @@
-const CACHE = 'lmp-v1';
-const FILES = ['./la-maison-propre-devis_1.html'];
+const CACHE = 'lmp-v2';
+const FILES = ['./la-maison-propre-devis.html'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(FILES))
-  );
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
   self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('./la-maison-propre-devis_1.html')))
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('./la-maison-propre-devis.html')))
   );
 });
